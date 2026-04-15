af4a1929e04367c7b464d42f2d193eeb25e6c876
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-15 22:53:14 +0530

    Create setlog.js

---
+++ commands/setlog.js
@@ -0,0 +1,45 @@
+const { EmbedBuilder } = require('discord.js');
+const checkPerms = require('../utils/permissions');
+const Guild = require('../models/guildSchema');
+
+module.exports = {
+  name: "setlog",
+
+  async execute(message, args) {
+    if (!checkPerms(message)) return;
+
+    const channel = message.mentions.channels.first();
+    if (!channel) {
+      return message.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle('<:error:1493997369505743000> Error')
+            .setDescription('Please mention a channel.')
+            .setColor('Red')
+        ]
+      });
+    }
+
+    let data = await Guild.findOne({ guildId: message.guild.id });
+
+    if (!data) {
+      data = new Guild({
+        guildId: message.guild.id,
+        logChannel: channel.id
+      });
+    } else {
+      data.logChannel = channel.id;
+    }
+
+    await data.save();
+
+    message.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle('✅ Log Channel Set')
+          .setDescription(`Logs will be sent to ${channel}`)
+          .setColor('Green')
+      ]
+    });
+  }
+};
