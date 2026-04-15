e499f75942182dc16a2031c6fc7643f7bc02eb49
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-15 23:19:18 +0530

    Create emoji.js

---
+++ utils/emoji.js
@@ -0,0 +1,76 @@
+module.exports = {
+  // Core
+  error: '<:error:1493997369505743000>',
+  success: '<:success:1493997365735198925>',
+  loading: '<:loading:1493997378707918921>',
+  warning: '<:warning:1493997372496416859>',
+
+  // General
+  user: '<:user:1493997318356209744>',
+  info: '<:info:1493997375365189813>',
+  search: '<:search:1493997330666360912>',
+  message: '<:message:1493997332776091690>',
+  settings: '<:settingsgamesanything:149399732591963186>',
+  config: '<:config:1493997268477411520>',
+
+  // Navigation
+  next: '<:next:1494006736321773679>',
+  previous: '<:previous:1494006746958790727>',
+  first: '<:first:1494006731808702595>',
+  last: '<:last:1494006733805326426>',
+  back: '<:back:1494018438627197069>',
+  close: '<:close:1494006774968221858>',
+
+  // Status
+  online: '<:online:1494006752197218354>',
+  offline: '<:offline:1494006757423321231>',
+  pinggood: '<:pinggood:1494012119497310329>',
+  pingbad: '<:pingbad:1494012114145120437>',
+  pinglow: '<:piglow:1494012117005897898>',
+
+  // Moderation
+  mod: '<:mod:1493997355089793085>',
+  ban: '<:ban:1493997277633843322>',
+  kick: '<:kick:1493997280141775011>',
+  mute: '<:mute:1493997284986195968>',
+  unmute: '<:unmute:1493997282440511539>',
+  warn: '<:warning:1493997372496416859>',
+  lock: '<:lock:1493997287746043944>',
+  unlock: '<:unlock:1493997290136932494>',
+  clear: '<:clear:1493997296138850434>',
+
+  // Utility
+  avatar: '<:avtar:1493997315814461550>',
+  server: '<:serverinfo:1493997271283662848>',
+  roles: '<:roles:1493997320650363070>',
+  members: '<:members:1493997335888396330>',
+  channels: '<:channels:1493997323431186502>',
+
+  // Fun / Games
+  dice: '<:cutedice:1494006789308678244>',
+  coin: '<:coinssssss:1494006785999372449>',
+  rps: '<:rps:1493997338732007524>',
+  rock: '<:rock:1494016254166696088>',
+  paper: '<:rpchandpaper:1494006804110250044>',
+  scissors: '<:rpcscissors:1494006801291808970>',
+
+  // Extra visuals
+  star: '<:starrrr:1494006725165191268>',
+  sparkle: '<:sparkle:1494006729502101555>',
+  fire: '<:fireeee:1494006727513870406>',
+  dot: '<:dot:1494006723051257919>',
+  arrow: '<:arrow:1494006720064782578>',
+  question: '<:857608question:1493997352573206749>',
+  random: '<:random:1494006796275155035>',
+
+  // System
+  id: '<:id:1494006760069927123>',
+  owner: '<:owner:1494006713987236070>',
+  calendar: '<:calendar:1494006711705665706>',
+  clock: '<:clock:1494006762553085952>',
+
+  // Special
+  nuke: '<:nuke:1493997293135986808>',
+  voice: '<:voice:1493997266237919404>',
+  voicekick: '<:voicekick:1493997307832700939>'
+};
