bbfe61bc860b9373af0bbbbb6b42c33ed5563709
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-16 16:26:51 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,62 +4,59 @@ const {
 } = require('discord.js');
 
 const embed = require('../utils/embed');
-const emoji = require('../utils/emoji');
 
 module.exports = {
   name: "help",
 
   async execute(message) {
+
     const base = embed.info(
       message,
       "Help Panel",
       "Select a category below"
     );
 
+    // ⚠️ USING NORMAL EMOJIS (100% SAFE)
     const menu = new StringSelectMenuBuilder()
       .setCustomId('help_menu')
-      .setPlaceholder('Choose category')
       .addOptions([
         {
           label: 'Moderation',
           value: 'mod',
-          emoji: emoji.mod
+          emoji: '🛡️'
         },
         {
           label: 'Utility',
           value: 'util',
-          emoji: emoji.settings
+          emoji: '⚙️'
         }
       ]);
 
-    const row = new ActionRowBuilder().addComponents(menu);
-
     const msg = await message.reply({
       embeds: [base],
-      components: [row]
+      components: [new ActionRowBuilder().addComponents(menu)]
     });
 
     const collector = msg.createMessageComponentCollector({ time: 0 });
 
     collector.on('collect', async i => {
       if (i.user.id !== message.author.id)
-        return i.reply({ content: `${emoji.error} Not yours`, ephemeral: true });
+        return i.reply({ content: "Not yours", ephemeral: true });
 
       if (i.values[0] === 'mod') {
         return i.update({
-          embeds: [
-            embed.info(message, "Moderation", "`.warn`\n`.warns`\n`.setlog`")
-          ]
+          embeds: [embed.info(message, "Moderation", ".warn\n.warns\n.setlog")]
         });
       }
 
       if (i.values[0] === 'util') {
         return i.update({
-          embeds: [
-            embed.info(message, "Utility", "`.avatar`\n`.list`")
-          ]
+          embeds: [embed.info(message, "Utility", ".avatar\n.list")]
         });
       }
+    });
+  }
+};
     });
   }
 };
