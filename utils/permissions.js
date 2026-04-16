b5299c0186990f4b134ec11087235660c59a32c0
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-16 16:35:35 +0530

    Update permissions.js

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,17 +1,5 @@
-const { EmbedBuilder, PermissionsBitField } = require('discord.js');
-
-module.exports = (message) => {
-  if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-
-    const embed = new EmbedBuilder()
-      .setTitle('<:error:1493997369505743000> Error')
-      .setDescription('You don’t have permission to use this command.')
-      .setColor('Red')
-      .setFooter({ text: `Requested by ${message.author.tag}` });
-
-    message.reply({ embeds: [embed] });
-    return false;
+module.exports = {
+  admin: (message) => {
+    return message.member.permissions.has("Administrator");
   }
-
-  return true;
 };
