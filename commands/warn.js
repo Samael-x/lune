2630fafce0d4d27dc5c5cd8c1ff0cebaeb327203
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-16 16:34:00 +0530

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,29 +1,28 @@
-const warnSchema = require('../models/warnSchema');
 const embed = require('../utils/embed');
+const permissions = require('../utils/permissions');
+const Warn = require('../models/warnSchema');
 
 module.exports = {
   name: "warn",
 
   async execute(message, args) {
-    if (!message.member.permissions.has("Administrator")) {
-      return message.reply({
-        embeds: [embed.error(message, "You don’t have permission")]
-      });
-    }
+
+    if (!permissions.admin(message))
+      return message.reply({ embeds: [embed.error(message, "No permission")] });
 
     const user = message.mentions.users.first();
     const reason = args.slice(1).join(" ") || "No reason";
 
-    if (!user) return;
+    if (!user) return message.reply({ embeds: [embed.error(message, "Mention user")] });
 
-    await warnSchema.create({
-      guildId: message.guild.id,
+    await Warn.create({
       userId: user.id,
+      guildId: message.guild.id,
       reason
     });
 
     message.reply({
-      embeds: [embed.success(message, "User Warned", `${user} → ${reason}`)]
+      embeds: [embed.success(message, `${user.tag} warned`)]
     });
   }
 };
