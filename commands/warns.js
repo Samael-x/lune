af56ee787ad44b519d1ae5e2598cab6e1ee9d224
Samael-x <117527732+Samael-x@users.noreply.github.com>
2026-04-16 16:34:35 +0530

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,43 +1,25 @@
-const warnSchema = require('../models/warnSchema');
-const pagination = require('../utils/pagination');
-const { EmbedBuilder } = require('discord.js');
-const emoji = require('../utils/emoji');
+const embed = require('../utils/embed');
+const Warn = require('../models/warnSchema');
 
 module.exports = {
   name: "warns",
 
   async execute(message) {
-    const user = message.mentions.users.first();
-    if (!user) return;
 
-    const data = await warnSchema.find({
-      guildId: message.guild.id,
-      userId: user.id
+    const user = message.mentions.users.first() || message.author;
+
+    const data = await Warn.find({
+      userId: user.id,
+      guildId: message.guild.id
     });
 
-    if (!data.length) {
-      return message.reply({
-        content: `${emoji.warning} No warnings`
-      });
-    }
+    if (!data.length)
+      return message.reply({ embeds: [embed.info(message, "Warns", "No warns")] });
 
-    const chunk = 5;
-    const pages = [];
+    let text = data.map((w, i) => `${i + 1}. ${w.reason}`).join("\n");
 
-    for (let i = 0; i < data.length; i += chunk) {
-      const current = data.slice(i, i + chunk);
-
-      const embed = new EmbedBuilder()
-        .setTitle(`${emoji.warn} Warnings for ${user.username}`)
-        .setDescription(
-          current.map((w, i) => `**${i + 1}.** ${w.reason}`).join('\n')
-        )
-        .setColor('#FEE75C')
-        .setFooter({ text: `Page ${Math.floor(i / chunk) + 1}` });
-
-      pages.push(embed);
-    }
-
-    pagination(message, pages);
+    message.reply({
+      embeds: [embed.info(message, `${user.tag} Warns`, text)]
+    });
   }
 };
